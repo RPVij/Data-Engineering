9a729c30d940acdbf745905ffa4835c7d5ebf436
Preetish <preetishvij@gmail.com>
2024-02-21 23:11:07 +0530

    SQL

--- SQL/Assignment-1.sql
+++ SQL/Assignment-1.sql
@@ -315,3 +315,25 @@ WHERE author_id=viewer_id
 ORDER BY author_id;
 
 -- Q19 --
+CREATE TABLE IF NOT EXISTS delivery
+(
+	 delivery_id INT
+	,customer_id INT
+	,order_date DATE
+	,customer_pref_delivery_date DATE
+	, CONSTRAINT pk PRIMARY KEY(delivery_id)
+)
+;
+
+INSERT INTO delivery VALUES
+	 (1,1,'2019-08-01','2019-08-02')
+	,(2,5,'2019-08-02','2019-08-02')
+	,(3,1,'2019-08-11','2019-08-11')
+	,(4,3,'2019-08-24','2019-08-26')
+	,(5,4,'2019-08-21','2019-08-22')
+	,(6,2,'2019-08-11','2019-08-13');
+	
+SELECT ROUND(SUM(order_date=customer_pref_delivery_date)*100/COUNT(*),2) AS immediate_orders_percentage
+FROM delivery;
+
+-- Q20 --
