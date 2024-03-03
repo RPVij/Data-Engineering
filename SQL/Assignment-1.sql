95aa2d804626929fe53640793490d5af2253c7dc
Preetish <preetishvij@gmail.com>
2024-03-04 00:03:27 +0530

    SQL

--- SQL/Assignment-1.sql
+++ SQL/Assignment-1.sql
@@ -1331,15 +1331,29 @@ INSERT INTO orders_48 VALUES
 ;
 
 SELECT
-	 b.book_id
-	,b.name
-    ,SUM(o.quantity)
-FROM books_48 b
-LEFT JOIN orders_48 o
-ON b.book_id = o.book_id
-WHERE b.available_from < '2019-05-23'
-GROUP BY b.book_id
-HAVING (SUM(o.quantity) < 10 OR SUM(o.quantity) = NULL)
+	 a.book_id
+    ,a.name
+    ,SUM(
+		CASE WHEN a.dispatch_date BETWEEN '2018-06-23' AND '2019-06-23' THEN a.quantity ELSE 0 END
+    ) AS quantity_order
+FROM
+(
+	SELECT
+		 b.book_id
+		,b.name
+		,o.quantity
+		,o.dispatch_date
+	FROM books_48 b
+	LEFT JOIN orders_48 o
+	ON b.book_id = o.book_id
+	WHERE b.available_from < '2019-05-23'
+) a
+GROUP BY
+     a.book_id
+    ,a.name
+HAVING SUM(
+		CASE WHEN a.dispatch_date BETWEEN '2018-06-23' AND '2019-06-23' THEN a.quantity ELSE 0 END
+    ) < 10
 ;
 
 -- Q49 --
