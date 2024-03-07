e42a84d29594dfa4630781250871605647f2ef3d
Preetish <preetishvij@gmail.com>
2024-03-07 07:15:49 +0530

    SQL-2

--- SQL/Assignment-2.sql
+++ SQL/Assignment-2.sql
@@ -495,3 +495,72 @@ FROM
 	) sales
 ) bestSellers
 WHERE bestSellers.rank_order = 1;
+
+-- Q66 --
+CREATE TABLE IF NOT EXISTS Product_66
+(
+    product_id INT,
+    product_name VARCHAR(50),
+    unit_price INT,
+    CONSTRAINT pk PRIMARY KEY (product_id)
+);
+INSERT INTO Product_66 VALUES
+    (1, 'S8', 1000),
+    (2, 'G4', 800),
+    (3, 'iPhone', 1400);
+
+CREATE TABLE IF NOT EXISTS Sales_66
+(
+    seller_id INT,
+    product_id INT,
+    buyer_id INT,
+    sale_date DATE,
+    quantity INT,
+    price INT,
+    CONSTRAINT fk_66 FOREIGN KEY (product_id) REFERENCES Product_66(product_id)
+);
+INSERT INTO Sales_66 VALUES
+    (1, 1, 1, '2019-01-21', 2, 2000),
+    (1, 2, 2, '2019-02-17', 1, 800),
+    (2, 1, 3, '2019-06-02', 1, 800),
+    (3, 3, 3, '2019-05-13', 2, 2800);
+	
+SELECT
+	s.buyer_id
+FROM Product_66 p
+INNER JOIN Sales_66 s
+ON p.product_id = s.product_id
+WHERE p.product_name = 'S8'
+AND s.buyer_id NOT IN (
+	SELECT
+		s.buyer_id
+	FROM Product_66 p
+	INNER JOIN Sales_66 s
+	ON p.product_id = s.product_id
+	WHERE p.product_name = 'iPhone'
+);
+
+-- Q67 --
+-- Create Customer Table
+CREATE TABLE IF NOT EXISTS Customer
+(
+    customer_id INT,
+    name VARCHAR(50),
+    visited_on DATE,
+    amount INT,
+    CONSTRAINT pk PRIMARY KEY (customer_id, visited_on)
+);
+
+-- Insert data into Customer Table
+INSERT INTO Customer VALUES
+    (1, 'Jhon', '2019-01-01', 100),
+    (2, 'Daniel', '2019-01-02', 110),
+    (3, 'Jade', '2019-01-03', 120),
+    (4, 'Khaled', '2019-01-04', 130),
+    (5, 'Winston', '2019-01-05', 110),
+    (6, 'Elvis', '2019-01-06', 140),
+    (7, 'Anna', '2019-01-07', 150),
+    (8, 'Maria', '2019-01-08', 80),
+    (9, 'Jaze', '2019-01-09', 110),
+    (1, 'Jhon', '2019-01-10', 130),
+    (3, 'Jade', '2019-01-10', 150);
